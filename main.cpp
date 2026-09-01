cf2ab1350c8016fce836b44ea4aed96a55068c6b
Jesus <321307792+DoctorJeffyReal@users.noreply.github.com>
2026-09-01 19:09:43 +0000

    added the hobby line and put reading comics

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
   // TODO: replace TODOs with YOUR labeled lines (5–6 total)
   std::cout << "Name: Jesus\n";
   std::cout << "Interest: Movies\n";
+  std::cout << "Hobby: Reading comics\n";
   std::cout << "CIS 5 goal: Learn C++ programming\n";
   std::cout << "Tool path: Github codespaces\n";
   std::cout << "Fun fact: I have double joint flexibility\n";
