ea4eb35fdb73982da635f050b7319f7b34890a41
Jesus <321307792+DoctorJeffyReal@users.noreply.github.com>
2026-09-01 19:04:29 +0000

    replace all todo placeholders and added my goal and Interest

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 
 int main() {
   // TODO: replace TODOs with YOUR labeled lines (5–6 total)
-  std::cout << "Name: TODO\n";
-  std::cout << "Interest: TODO\n";
-  std::cout << "CIS 5 goal: TODO\n";
-  std::cout << "Tool path: TODO\n";
-  std::cout << "Fun fact: TODO\n";
+  std::cout << "Name: Jesus\n";
+  std::cout << "Interest: Movies\n";
+  std::cout << "CIS 5 goal: Learn C++ programming\n";
+  std::cout << "Tool path: Github codespaces\n";
+  std::cout << "Fun fact: I have double joint flexibility\n";
   return 0;
 }
